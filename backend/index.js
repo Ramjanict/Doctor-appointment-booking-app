5f84ad304b97b452c19f66ac0592ddcad82b722e
ramjan <mdramjan.ict@gmail.com>
2024-11-05 07:39:11 +0600

    Doctor backend url change

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ connectCloudinary();
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 
 // API endpoints
 app.use("/api/admin", adminRouter);
