dbd6798ef4f93ffbd4e01e3f7ac6e64abb48c86b
Ramjan <mdramjan.ict@gmail.com>
2024-11-06 19:00:13 +0600

    admin url add

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,7 @@ connectCloudinary();
 
 //middleware
 app.use(express.json());
-app.use(
-  cors({ origin: process.env.FRONTEND_URL, origin: process.env.ADMIN_URL })
-);
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 
 // API endpoints
 app.use("/api/admin", adminRouter);
